efc08b914d303f74a6b0778dbbb1442c92ef7c49
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-03-21 22:08:05 -0400

    make the program actually quit
    so I or you don't have to use task manager

--- donut.py
+++ donut.py
@@ -96,6 +96,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            pygame.display.quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+                pygame.display.quit()
